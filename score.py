9df17ba4aaa7cb6f74ef8c96eb2de8e914d2779e
Choi_Hye_Min <chym0305@naver.com>
2024-12-15 17:44:51 +0900

    new file:   score.py 	new file:   students.json

---
+++ score.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+task_file='students.json'
+
+
+def load_task():
+    pass
+
+def save_task():
+    pass
+
+#1번 학생 입력하기 데스크/ 이름:--,성별:--,국어 수학 영어 과학 체육 점수:--
+
+#2번 학생 점수만 수정하기 데스크/ 
+
+#3번 학생 전체 성적 산출하기 데스크/
+
+#4번 학생 삭제하기 데스크
+
+#5번 프로그램 종료
